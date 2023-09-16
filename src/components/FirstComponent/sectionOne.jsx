47097ebd51dad5a0aeaee3dabdd64cec084fb3a9
Exploit34 <josefexner456@gmail.com>
2023-09-15 19:17:21 -0500

    porta10

--- src/components/FirstComponent/sectionOne.jsx
+++ src/components/FirstComponent/sectionOne.jsx
@@ -2,7 +2,6 @@ import { TextSectionOne } from "./textSectionOne.jsx"
 export const SectionOne = () => {
   return (
     <div id="text">
-      
       <img src="" alt="" id="imagen" />
       <TextSectionOne></TextSectionOne>
     </div>
